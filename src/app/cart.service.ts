0fa677c58a7157f3ddf3c4026abd4b7eb8de1b16
anand-paleja <apaleja19@gmail.com>
2021-07-22 17:42:49 -0400

    feat: add functionality to get shipping prices to the cart service

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 
 @Injectable()
 export class CartService {
   items: Product[] = [];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   /**
    * Adds a product to the cart
@@ -28,5 +29,12 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  /**
+   * Gets shipping prices via an http request.
+   */
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
 }
